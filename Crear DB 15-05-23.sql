c31072b271102e698f75e3c8af4b4e7eb4dda140
Cristian-Kakao <131078780+Cristian-Kakao@users.noreply.github.com>
2023-08-13 01:58:02 -0300

    Delete Crear DB 15-05-23.sql

--- Crear DB 15-05-23.sql
+++
@@ -1,9 +0,0 @@
-CREATE database BD_EJEMPLO_INNOVA;
--- Así se hacen los comentarios
-USE BD_EJEMPLO_INNOVA;
-CREATE table PERSONA(
-	DNI int,
-	Nombre varchar(30),
-	Direccion varchar(30),
-	Telefono int);
-    
